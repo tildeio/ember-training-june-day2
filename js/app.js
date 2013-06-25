2929b3906a196d07eb51de7c2d101a062917b5f3
Tom Dale <tom@tomdale.net>
2013-06-25 08:34:17 -0400

    Step 8

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(function() {
+"use strict";
+
+window.App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('album', { path: '/album/:album_id' });
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.ALBUM_FIXTURES;
+  }
+});
+
+App.AlbumRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
+  }
+});
+
+Ember.Handlebars.helper('format-duration', function(seconds) {
+  var minutes = Math.floor(seconds/60);
+  var remainingSeconds = seconds % 60;
+
+  var result = '';
+  if (remainingSeconds < 10) {
+    result = "0";
+  }
+
+  result += String(remainingSeconds);
+
+  result = minutes + ":" + result;
+
+  return result;
+});
+
+})();
